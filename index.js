fb4cdba1a3a75ef72f6e2b2588b006dd7250faab
steveruu <prchalek@gmail.com>
2022-10-27 19:15:31 +0200

    final commit, nejde to, spotify api je uplne na nic basically a ja nejsem dostatečně placenej na to, abych tady dělal, že pokaždý co se ten script spustí, tak se swapne token protože to dělaj multimiliardový firmy a ne selflearnt 14yo devs.

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 const axios = require('axios').default;
+const XMLHttpRequest = require('xhr2');
 const https = require('https');
 const url = "https://api.spotify.com/artists/v1/";
 var artistID;
+var xhr = new XMLHttpRequest();
 
 var artistIDs = ["4NOFcRCgjvnRy8nKVGUM0L"] // steveruu artist URI
 artistID = artistIDs[0]
 
 var fullURL = url + artistID;
-const token = '81622b79b05140edb4ae346de740b27c';
+const token = 'af1a22f02ecc4371bec205f62966ebb9';
 
-const options = {
-  headers: {
-    "Authorization": 'Bearer' + token
-  }
-};
+function requestArtist() {
+  xhr.open("GET", url);
+  xhr.setRequestHeader("Authorization", `Bearer ${token}`);
 
-async function request(){
-  try {
-    const res = await axios.get(fullURL, {
-      headers: {
-        'Authorization': `Bearer ${token}`
-      }});} 
-    catch(err) {
-      console.log(err);
-  }
-  console.log(this.res);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        console.log(xhr.status);
+        console.log(xhr.responseText);
+    }};
+
+  xhr.send();
+}
+
+function swapToken() {
+xhr.open("POST", "https://api.spotify.com/v1/swap")
+xhr.setRequestHeader("Content-Type", 'application/x-www-form-urlencoded')
+xhr.setRequestHeader("Authorization", `Bearer ${token}`);
+
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+  }};
+
+xhr.send();
 }
 
 
-request();
+swapToken();
+
