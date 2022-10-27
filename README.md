a4fcab774719571075133fbaf8967f717c6c35e6
steveruu <prchalek@gmail.com>
2022-10-27 17:52:57 +0200

    Error: connect ECONNREFUSED 127.0.0.1:443

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ spotify czech artist sorting algorithm
 
 # what am i doing
 - this is pain pain pain pain
-* why can't i use ajax in node
+- why can't i use ajax in node
 - pain eternal pain eternal pain
 - i hate working with APIs 
--- index.js
+++ index.js
@@ -6,15 +6,25 @@ var artistIDs = ["4NOFcRCgjvnRy8nKVGUM0L"] // steveruu artist URI
 artistID = artistIDs[0]
 
 var fullURL = url + artistID;
+const token = 'af1a22f02ecc4371bec205f62966ebb9';
 
 var options = {
     url: fullURL,
     method: 'GET',
     dataType: 'json',
-    token: af1a22f02ecc4371bec205f62966ebb9,
     headers: {
         "Authorization": 'Bearer' + token // JÁ NEVIM JESTLI TO CHCE CLIENT ID NEBO CLIENT SECRET
     }   
 };
 
-https.get(
+https.get(options, (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
