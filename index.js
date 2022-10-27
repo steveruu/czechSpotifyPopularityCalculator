96e72624de9dbfe97c306e0303938d5ff03adc67
steveruu <prchalek@gmail.com>
2022-10-27 18:11:44 +0200

    Cannot read properties of undefined (reading 'get')

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const axios = require('axios').default;
+const { error } = require('console');
 const https = require('https');
 const url = "https://api.spotify.com/artists/v1/";
 var artistID;
@@ -6,25 +8,23 @@ var artistIDs = ["4NOFcRCgjvnRy8nKVGUM0L"] // steveruu artist URI
 artistID = artistIDs[0]
 
 var fullURL = url + artistID;
-const token = 'af1a22f02ecc4371bec205f62966ebb9';
+const token = '81622b79b05140edb4ae346de740b27c';
 
-var options = {
-    url: fullURL,
-    method: 'GET',
-    dataType: 'json',
-    headers: {
-        "Authorization": 'Bearer' + token // JÁ NEVIM JESTLI TO CHCE CLIENT ID NEBO CLIENT SECRET
-    }   
-};
+const config = {
+  headers: {
+    "Authorization": 'Bearer' + token
+  }
+}
+async function request(){
+  try {
+    const res = await axios.get(fullURL, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }});} 
+    catch(err) {
+      console.log(err);
+  }
+  console.log(this.res);
+}
 
-https.get(options, (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
-
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
-
-}).on('error', (e) => {
-  console.error(e);
-});
+request()
